75b44c85c8836842ff5aaf97242bd377d0189820
fatmanurul <fatmanurulhidayah347@gmail.com>
2024-03-28 15:09:54 +0700

    add authenticate

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,32 @@ use App\Http\Controllers\registerController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// auth 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+
+
+
+
+
+
+
+
 Route::get('/photos', [GalerryController::class, 'index']);
 Route::get('/category', [GalerryController::class, 'photos']);
 Route::get('/admin/daftar', [GalerryController::class, 'admin']);
 Route::get('/admin/foto', [GalerryController::class, 'foto']);
 Route::get('/admin/foto/create', [GalerryController::class, 'create']);
-Route::get('/login', [loginController::class, 'index']);
-Route::get('/register', [loginController::class, 'register']);
+// Route::get('/login', [loginController::class, 'index']);
+// Route::get('/register', [loginController::class, 'register']);
 
 //route auth
 // Route::get('/login', [loginController::class ,'index'])->name('login');
